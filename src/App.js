8d19b2b089e3c7b688cf9dddcfdc16a54962b777
DillanR1 <dillanroby@yahoo.com>
2020-11-10 15:57:43 -0800

    Alert component added. Bug fixed. Alert was not being exported properly

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Navbar from "./components/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/Alert";
 import axios from "axios";
 import "./App.css";
 
@@ -9,6 +10,7 @@ class App extends Component {
   state = {
     users: [],
     loading: false,
+    alert: null,
   };
 
   // async componentDidMount() {
@@ -28,7 +30,15 @@ class App extends Component {
     this.setState({ users: res.data.items, loading: false });
   };
 
+  // Clear Users
   clearUsers = () => this.setState({ users: [], loading: false });
+
+  // Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } });
+
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
 
   render() {
     const { users, loading } = this.state;
@@ -37,10 +47,12 @@ class App extends Component {
       <div className="App">
         <Navbar />
         <div className="container">
+          <Alert alert={this.state.alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={this.state.users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>

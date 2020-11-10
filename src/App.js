8b3bacdf26167fd2abd4a3633d4a8a4619b83ee0
DillanR1 <dillanroby@yahoo.com>
2020-11-10 00:02:06 -0800

    Clear users btn works, only appears when there are users to clear. Destructured App.js

--- src/App.js
+++ src/App.js
@@ -1,42 +1,52 @@
-import React, { Component } from 'react';
-import Navbar from './components/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
-import axios from 'axios';
-import './App.css';
+import React, { Component } from "react";
+import Navbar from "./components/Navbar";
+import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import axios from "axios";
+import "./App.css";
 
 class App extends Component {
   state = {
     users: [],
     loading: false,
-  }
+  };
 
   // async componentDidMount() {
   //   this.setState({ loading: true });
-    
+
   //   const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-      
+
   //   this.setState({ users: res.data, loading: false });
   // }
-  
-  searchUsers = async text => {
+
+  searchUsers = async (text) => {
     this.setState({ loading: true });
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-      
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
     this.setState({ users: res.data.items, loading: false });
   };
 
+  clearUsers = () => this.setState({ users: [], loading: false });
+
   render() {
-    return (  
+    const { users, loading } = this.state;
+
+    return (
       <div className="App">
-        <Navbar /> 
+        <Navbar />
         <div className="container">
-          <Search searchUsers={this.searchUsers} />
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Search
+            searchUsers={this.searchUsers}
+            clearUsers={this.clearUsers}
+            showClear={this.state.users.length > 0 ? true : false}
+          />
+          <Users loading={loading} users={users} />
         </div>
       </div>
     );
   }
 }
- 
+
 export default App;

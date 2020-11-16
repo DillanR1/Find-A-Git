ca7c956e1953ee4be953548ceeaa4d59b9756052
DillanR1 <dillanroby@yahoo.com>
2020-11-15 23:04:28 -0800

    refactored Search.js to functional components

--- src/components/repos/Repos.js
+++ src/components/repos/Repos.js
@@ -1,13 +1,13 @@
 import React from 'react'
-import PropTypes from 'prop-types';
+import propTypes from 'prop-types';
 import RepoItem from './RepoItem';
 
 export const Repos = ({ repos }) => {
     return repos.map(repo => <RepoItem repo={repo} key={repo.id} />)    
 }
 
-Repos.PropTypes = {
-    repos: PropTypes.array.isRequired
+Repos.propTypes = {
+    repos: propTypes.array.isRequired
 };
 
 export default Repos;
